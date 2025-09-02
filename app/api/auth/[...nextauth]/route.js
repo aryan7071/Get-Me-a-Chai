407ac1314c461006662685e5e69f3f4fc3315a0e
aryan7071 <aryan002005@gmail.com>
2025-09-03 01:29:02 +0530

    db

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,32 +4,64 @@ import NextAuth from 'next-auth'
 // import GoogleProvider from 'next-auth/providers/google'
 // import EmailProvider from 'next-auth/providers/email'
 import GitHubProvider from 'next-auth/providers/github'
+import mongoose from 'mongoose'
+import User from '@/models/User'
+import Payment from '@/models/Payment'
+import connectDB from '@/db/connectDB'
 
 export const authoptions = NextAuth({
-    providers: [
-        // OAuth authentication providers...
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        }),
-        // AppleProvider({
-        //     clientId: process.env.APPLE_ID,
-        //     clientSecret: process.env.APPLE_SECRET
-        // }),
-        // FacebookProvider({
-        //     clientId: process.env.FACEBOOK_ID,
-        //     clientSecret: process.env.FACEBOOK_SECRET
-        // }),
-        // GoogleProvider({
-        //     clientId: process.env.GOOGLE_ID,
-        //     clientSecret: process.env.GOOGLE_SECRET
-        // }),
-        // // Passwordless / email sign in
-        // EmailProvider({
-        //     server: process.env.MAIL_SERVER,
-        //     from: 'NextAuth.js <no-reply@example.com>'
-        // }),
-    ]
+  providers: [
+    // OAuth authentication providers...
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    }),
+    // AppleProvider({
+    //     clientId: process.env.APPLE_ID,
+    //     clientSecret: process.env.APPLE_SECRET
+    // }),
+    // FacebookProvider({
+    //     clientId: process.env.FACEBOOK_ID,
+    //     clientSecret: process.env.FACEBOOK_SECRET
+    // }),
+    // GoogleProvider({
+    //     clientId: process.env.GOOGLE_ID,
+    //     clientSecret: process.env.GOOGLE_SECRET
+    // }),
+    // // Passwordless / email sign in
+    // EmailProvider({
+    //     server: process.env.MAIL_SERVER,
+    //     from: 'NextAuth.js <no-reply@example.com>'
+    // }),
+  ],
+  
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      if (account.provider == "github") {
+        
+        //Check if the  user is already in the database
+        const currentUser = User.findOne({ email: email })
+        if(!currentUser){
+          //Create a new user
+          const newUser= new User({
+            email:email,
+            username:email.split("@")[0],
+          })
+          await newUser.save()
+         
+        }
+        else{
+          user.name = currentUser.username
+        }
+        return true
+      }
+    },
+     async session({ session, user, token }) {
+      const dbUser = await User.find({email:session.user.email})
+       session.user.name = dbUser.username
+      return session
+    },
+  }
 })
 
 
